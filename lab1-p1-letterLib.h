f19f22e2f2e971ecef51fbd6148d7decaf4e8cd5
fryland001 <47129700+fryland001@users.noreply.github.com>
2019-03-06 11:29:58 -0600

    Update lab1-p1-letterLib.h

--- lab1-p1-letterLib.h
+++ lab1-p1-letterLib.h
@@ -12,7 +12,7 @@
  * Character 7 will be created in programs chosen when using this header
  *
  */
- void drawChar7(double ulCornerX, double ulCornerY, float height)
+ void drawChar7(double ulCornerX, double ulCornerY, double height)
  {
       double ulY = ulCornerY;
       double ulX = ulCornerX;
