c25a92871cc0f9bce137eebd0bf6f95dcbe74162
fryland001 <47129700+fryland001@users.noreply.github.com>
2019-03-06 11:25:12 -0600

    Updated for uniformity
    Hope this helps

--- lab1-p1-letterLib.h
+++ lab1-p1-letterLib.h
@@ -5,34 +5,34 @@
 *
 * Author fryland001 1/29/2019
 */
- #include "gfx2.h"
+  #include "gfx2.h"
  
  /* drawChar7
  *
- * Charater 7 will be created in programs choosen when using this header
+ * Character 7 will be created in programs chosen when using this header
  *
  */
- void drawChar7(double width, double height, size)
+ void drawChar7(double ulCornerX, double ulCornerY, height)
  {
-      double ulCornerY = height;
-      double ulCornerX = width;
-      double urCornerY = height;
-      double urCornerX = width + (0.75*size);
-      double lrCornerY = height + size;
-      double lrCornerX = width + (0.5*size); 
-      double llCornerY = height+ size;
-      double llCornerX = width + (0.2*size);
-      double mrCornerY = height + (0.25*size);
-      double mrCornerX = width + (0.4*size);      
-      double mlCornerY = height + (0.25*size);
-      double mlCornerX = width;
+      double ulY = ulCornerY;
+      double ulX = ulCornerX;
+      double urY = ulCornerY;
+      double urX = ulCornerX + (0.75*height);
+      double lrY = ulCornerY + height;
+      double lrX = ulCornerX + (0.5*height); 
+      double llY = ulCornerY + height;
+      double llX = ulCornerX + (0.2*height);
+      double mrY = ulCornerY + (0.25*height);
+      double mrX = ulCornerX + (0.4*height);      
+      double mlY = ulCornerY + (0.25*height);
+      double mlX = ulCornerX;
       
-      gfx_line(ulCornerX,ulCornerY,urCornerX,urCornerY);
-      gfx_line(urCornerX,urCornerY,lrCornerX,lrCornerY);
-      gfx_line(lrCornerX,lrCornerY,llCornerX,llCornerY);
-      gfx_line(llCornerX,llCornerY,mrCornerX,mrCornerY);
-      gfx_line(mrCornerX,mrCornerY,mlCornerX,mlCornerY);
-      gfx_line(mlCornerX,mlCornerY,ulCornerX,ulCornerY);
+      gfx_line(ulX,ulY,urX,urY);
+      gfx_line(urX,urY,lrX,lrY);
+      gfx_line(lrX,lrY,llX,llY);
+      gfx_line(llX,llY,mrX,mrY);
+      gfx_line(mrX,mrY,mlX,mlY);
+      gfx_line(mlX,mlY,ulX,ulY);
       
  }
  
